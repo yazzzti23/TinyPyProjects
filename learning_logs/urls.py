ecda333b9936213d94f22965580a42423ac3891d
yazzzti <yashtimittal123@gmail.com>
2025-11-08 17:14:19 +0530

    first commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""defines url patterns for learning logs"""
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns =[
+    #home page
+    path('',views.index, name='index'),
+    #page that shows all topics
+    path('topics/',views.topics, name='topics'),
+    #detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic,name='topic'),
+]
