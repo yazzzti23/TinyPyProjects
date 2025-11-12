2cf10ced19a442553bccae4469797cb155c38c5a
yazzzti <yashtimittal123@gmail.com>
2025-11-12 10:17:34 +0530

    forms 2

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -31,4 +31,45 @@ def new_topic(request):
         form = TopicForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('learning_logs:topics')
+            return redirect('learning_logs:topics')#redirect function 
+        #display a blank or invalid form 
+    context = {'form':form}
+    return render(request, 'learning_logs/new_topic.html',context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form =  EntryForm()
+    else:
+        #Post data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)#still havent saved it in the database yet cause we need to attach it to the topic 
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    #display a blank or invalid form
+    context = {'topic':topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html',context)
+
+def edit_entry(request, entry_id):
+    """edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic# we are setting this topic for later so that we can redirect back to topic 
+
+    if request.method !=  'POST':
+        #initial request; pre-fill form with the current entry
+        form = EntryForm(instance=entry)#means a build a form using this specific antry as the source so all current values appear in the inputs 
+    else:
+        #POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)#redirecting to topics page 
+        
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
